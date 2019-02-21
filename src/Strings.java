d957f008feeda55f61f5a5d6c3b723b72b6733dc
pointschool <koshurinova@gmail.com>
2019-02-21 12:42:16 +0300

    уроки java

---
+++ src/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String [] args) {
+        int x=5;
+        String s="Hello";
+        String space=" ";
+        String name="Mila";
+        System.out.println(s+space+name+"!");
+        System.out.println(name+space+"gets"+space+x+" hw");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String [] args) {
+        int myInt; //целые числа 32 бит
+        myInt=5;
+        short myShort=31999; //целые небольшие числа 16 бит -32 тыс ... 32 тыс
+        long myLong = 257000; // целые большие числа 64 бит
+
+        double myDouble = 5.435; // вещественные числа 64 бит
+        float myFloat = 5.435f; // вещественные числа 32 бит, в конце ставим f
+
+        char myChar= 'd'; // символ
+
+        boolean myBoolen=true; // логические true/false
+
+        byte myByte=127; // 1 байт информации -128...127
+
+    }
+}
